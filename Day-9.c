749094bb2f0c5f9ce5d217008871b3e069ba2bf2
AnubhavVarshney23 <anubhav23varshney@gmail.com>
2026-03-04 22:43:32 +0530

    Create Day-9.c

---
+++ Day-9.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+        char code[50],rev[50];
+        int len=0, i;
+        printf("enter the code(no spaces): ");
+        scanf("%s", code);       //no spaces 
+        for (i= 0; code[i] != '\0' ; i++)
+        {
+                len++;
+        }
+        for (i=len-1; i>=0; i--)
+        {
+                printf("%c", code[i]);
+        }
+        printf("\n");
+        return 0;
+}
