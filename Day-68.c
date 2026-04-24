3732585eb16999c37b11c3f31ce45c529052e812
AnubhavVarshney23 <anubhav23varshney@gmail.com>
2026-04-24 23:21:39 +0530

    Create Day-68.c

---
+++ Day-68.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 100
+int main() {
+    int n, m;
+    int adj[MAX][MAX] = {0};
+    int indegree[MAX] = {0};
+    int queue[MAX], front = 0, rear = -1;
+    int topo[MAX], index = 0;
+
+    scanf("%d", &n);
+    scanf("%d", &m);
+
+    for(int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1;
+        indegree[v]++;
+    }
+
+    for(int i = 0; i < n; i++) {
+        if(indegree[i] == 0) {
+            queue[++rear] = i;
+        }
+    }
+
+    while(front <= rear) {
+        int node = queue[front++];
+        topo[index++] = node;
+
+        for(int i = 0; i < n; i++) {
+            if(adj[node][i] == 1) {
+                indegree[i]--;
+                if(indegree[i] == 0) {
+                    queue[++rear] = i;
+                }
+            }
+        }
+    }
+
+    if(index != n) {
+        printf("Cycle detected");
+    } else {
+        for(int i = 0; i < n; i++) {
+            printf("%d ", topo[i]);
+        }
+    }
+
+    return 0;
+}
